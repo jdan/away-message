61b7aa14e65ac5ed6ca85b22438fb4c6b8403cd3
Jordan Scales <scalesjordan@gmail.com>
2023-12-26 17:08:19 -0500

    light iconography

---
+++ app/icons.tsx
@@ -0,0 +1,33 @@
+export const TextColorIcon = (props: { fill: string }) => (
+  <svg
+    width="20"
+    height="14"
+    viewBox="0 0 20 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M8 3H7V4H6V5V6H5V7V8H4V9V10H3V11H4H5H6H7V10H6V9V8H7H8H9V9V10H8V11H9H10H11H12V10H11V9V8H10V7V6H9V5V4H8V3ZM7 6H8V7H7V6Z"
+      fill={props.fill}
+    />
+  </svg>
+);
+
+export const BackgroundColorIcon = (props: { fill: string }) => (
+  <svg
+    width="20"
+    height="14"
+    viewBox="0 0 20 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M15 2H2V12H15V2ZM8 3H9V4H10V5V6H11V7V8H12V9V10H13V11H12H11H10H9V10H10V9V8H9H8H7V9V10H8V11H7H6H5H4V10H5V9V8H6V7V6H7V5V4H8V3ZM9 6H8V7H9V6Z"
+      fill={props.fill}
+    />
+  </svg>
+);
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CSSProperties, useMemo, useState } from "react";
+import { BackgroundColorIcon, TextColorIcon } from "./icons";
 
 interface EditorStateV1 {
   version: 1;
@@ -13,6 +14,31 @@ const fontSizes = [8, 10, 14, 18, 24, 32, 64];
 
 type EditorState = EditorStateV1;
 
+const IconButton = (props: { children: React.ReactNode }) => (
+  <button
+    className="icon-button"
+    style={{
+      padding: "2px 0 0 0",
+      minWidth: 0,
+      minHeight: 0,
+    }}
+  >
+    {props.children}
+  </button>
+);
+
+const Divider = () => (
+  <div
+    style={{
+      display: "inline-block",
+      height: 18,
+      width: 2,
+      boxShadow: "inset -1px 0 #fff, inset 1px 0 grey",
+      margin: "2px 6px 0 6px",
+    }}
+  />
+);
+
 export default function Home() {
   const [state, setState] = useState<EditorState>({
     version: 1,
@@ -20,6 +46,16 @@ export default function Home() {
     fontFamily: "Times New Roman",
     value: `BRB mom needs computer lol "chocolate milk?" HAHAHA derek`,
   });
+
+  const style = useMemo<CSSProperties>(
+    () => ({
+      fontFamily: state.fontFamily,
+      overflowY: "auto",
+      height: 64,
+      fontSize: state.fontSize,
+    }),
+    [state.fontFamily, state.fontSize]
+  );
 
   const increaseFontSize = () => {
     const idx = fontSizes.indexOf(state.fontSize);
@@ -33,15 +69,9 @@ export default function Home() {
     setState({ ...state, fontSize: fontSizes[idx - 1] });
   };
 
-  const style = useMemo<CSSProperties>(
-    () => ({
-      fontFamily: state.fontFamily,
-      overflowY: "auto",
-      height: 64,
-      fontSize: state.fontSize,
-    }),
-    [state.fontFamily, state.fontSize]
-  );
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setState({ ...state, value: e.target.value });
+  };
 
   return (
     <main>
@@ -64,12 +94,21 @@ export default function Home() {
           <section className="field-row-stacked message-input">
             <label>Enter new Away message:</label>
             <fieldset className="editing-controls">
-              <button onClick={() => increaseFontSize()}>{"+"}</button>
-              <button onClick={() => decreaseFontSize()}>{"-"}</button>
+              <IconButton>
+                <TextColorIcon fill="#1618FD" />
+              </IconButton>
+              <IconButton>
+                <BackgroundColorIcon fill="#1618FD" />
+              </IconButton>
+              <Divider />
+              <IconButton>
+                <TextColorIcon fill="#1618FD" />
+              </IconButton>
             </fieldset>
             <textarea
               spellCheck="false"
-              defaultValue={state.value}
+              value={state.value}
+              onChange={handleChange}
               style={style}
             />
           </section>
