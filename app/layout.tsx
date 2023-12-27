e6c61ebdaf934739f42f2cf6d492124587736ed4
Jordan Scales <scalesjordan@gmail.com>
2023-12-27 11:22:18 -0500

    viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import "98.css";
 import "./style.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Edit Away Message",
@@ -15,6 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+      </Head>
       <body>{children}</body>
     </html>
   );
