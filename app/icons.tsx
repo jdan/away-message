6eaca0f096e52abda9d1ab009705c03f06698050
Jordan Scales <scalesjordan@gmail.com>
2023-12-27 09:27:43 -0500

    svg attributes

--- app/icons.tsx
+++ app/icons.tsx
@@ -7,8 +7,8 @@ export const TextColorIcon = (props: { fill: string }) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M8 3H7V4H6V5V6H5V7V8H4V9V10H3V11H4H5H6H7V10H6V9V8H7H8H9V9V10H8V11H9H10H11H12V10H11V9V8H10V7V6H9V5V4H8V3ZM7 6H8V7H7V6Z"
       fill={props.fill}
     />
@@ -24,8 +24,8 @@ export const BackgroundColorIcon = (props: { fill: string }) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M15 2H2V12H15V2ZM8 3H9V4H10V5V6H11V7V8H12V9V10H13V11H12H11H10H9V10H10V9V8H9H8H7V9V10H8V11H7H6H5H4V10H5V9V8H6V7V6H7V5V4H8V3ZM9 6H8V7H9V6Z"
       fill={props.fill}
     />
@@ -44,20 +44,20 @@ export const SmallerFontIcon = (props: {
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M6 7H5V8V9H4V10V11H3V12H4H5V11V10H6V11V12H7H8V11H7V10V9H6V8V7Z"
       fill={props.letterFill}
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M11 4H10V5V6V7H11H12H13H14H15V6V5V4H14H13H12H11ZM13 5H14V6H13V5ZM12 6H11V5H12V6Z"
       fill={props.trailFill}
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M11 7H10V8V9V10H11V11H12V12H13V11H14V10H15V9V8V7H14H13V8H12V7H11Z"
       fill={props.letterFill}
     />
@@ -73,8 +73,8 @@ export const NormalFontIcon = (props: { fill: string }) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M9 4H8V5H7V6V7H6V8V9H5V10V11H4V12H5H6H7H8V11H7V10V9H8H9H10V10V11H9V12H10H11H12H13V11H12V10V9H11V8V7H10V6V5H9V4ZM8 7H9V8H8V7Z"
       fill={props.fill}
     />
@@ -93,20 +93,20 @@ export const LargerFontIcon = (props: {
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M3 7H2V8V9V10H3H4H5H6H7V9V8V7H6H5H4H3ZM5 8H6V9H5V8ZM4 9H3V8H4V9Z"
       fill={props.trailFill}
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M4 2H5V3H6V4H7V5V6V7H6V6H5V7H4V6H3V7H2V6V5V4H3V3H4V2Z"
       fill={props.letterFill}
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M12 2H11V3V4H10V5V6H9V7V8V9H8V10V11H7V12H8H9H10H11V11H10V10V9H11H12H13V10V11H12V12H13H14H15H16V11H15V10V9H14V8V7V6H13V5V4H12V3V2ZM11 6H12V7V8H11V7V6Z"
       fill={props.letterFill}
     />
@@ -122,8 +122,8 @@ export const BoldIcon = (props: { fill: string }) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M5 3H4V4H5V5V6V7V8V9V10V11H4V12H5H6H7H8H9H10H11V11H12V10V9V8H11H10V7H11H12V6V5V4H11V3H10H9H8H7H6H5ZM9 7V6V5V4H8V5V6V7H9ZM8 8H9V9V10V11H8V10V9V8Z"
       fill={props.fill}
     />
@@ -139,8 +139,8 @@ export const ItalicIcon = (props: { fill: string }) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M10 3H9V4H10H11V5H10V6H9V7H8V8H7V9H6V10H5V11H4H3V12H4H5H6H7H8H9V11H8H7V10H8V9H9V8H10V7H11V6H12V5H13V4H14H15V3H14H13H12H11H10Z"
       fill={props.fill}
     />
@@ -156,8 +156,8 @@ export const UnderlineIcon = (props: { fill: string }) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M5 3H6H7H8V4V5V6V7V8V9H9H10H11V8V7V6V5V4H10V3H11H12H13V4V5V6V7V8V9V10H12H11H10H9H8H7V9H6V8V7V6V5V4H5V3ZM9 11H8H7H6H5V12H6H7H8H9H10H11H12H13H14V11H13H12H11H10H9Z"
       fill={props.fill}
     />
